a8f2f5c8916f8e9d7812355f173ecc39697b2c2c
Michael Zwierzanski <69387856+mz106@users.noreply.github.com>
2024-07-15 15:52:31 +0100

    feature: add login route users

--- src/users/controllers.js
+++ src/users/controllers.js
@@ -4,9 +4,25 @@ const register = async (req, res) => {
   try {
     const user = await User.create(req.body);
 
-    console.log(user);
-
     res.status(201).json({ message: "success", user });
+  } catch (error) {
+    res.status(500).json({ message: error.message, error: error });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    res.status(201).json({ message: "success", username: req.user.username });
+  } catch (error) {
+    res.status(500).json({ message: error.message, error: error });
+  }
+};
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.findAll({});
+
+    res.status(200).json({ message: "success", users });
   } catch (error) {
     res.status(500).json({ message: error.message, error: error });
   }
@@ -14,4 +30,6 @@ const register = async (req, res) => {
 
 module.exports = {
   register,
+  login,
+  getAllUsers,
 };
--- src/users/routes.js
+++ src/users/routes.js
@@ -1,8 +1,11 @@
 const { Router } = require("express");
 const userRouter = Router();
 
-const { register } = require("./controllers");
+const { register, login, getAllUsers } = require("./controllers");
+const { validateRegister, validateLogin } = require("../middleware/validation");
 
-userRouter.post("/register", register);
+userRouter.post("/register", validateRegister, register);
+userRouter.post("/login", validateLogin, login);
 
+userRouter.get("/getAllUsers", getAllUsers);
 module.exports = userRouter;
